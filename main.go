0d1e80abb0b3a1799fe0d814ff58bf3609bc8d90
natdempk <dempkowski.n@husky.neu.edu>
2016-12-08 16:18:39 -0500

    More work

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"os"
 	"time"
+
+	"github.com/satori/go.uuid"
 )
 
 type Message struct {
@@ -24,17 +26,33 @@ type Message struct {
 	LastLogTerm  int    `json:"lastLogTerm"`
 	VoteGranted  bool   `json:"voteGranted"`
 	// AppendEntries RPC
+	PrevLogIndex int       `json:"prevLogIndex"`
+	PrevLogTerm  int       `json:"prevLogTerm"`
+	Entries      []LogItem `json:"entries"`
+	LeaderCommit int       `json:"leaderCommit"`
+	Success      bool      `json:"success"`
 }
 
 type LogItem struct {
-	Foo string
+	Key   string
+	Value string
+	Term  int
 }
 
 var conn net.Conn
 
+// constants
+const LEADER = "leader"
+const FOLLOWER = "follower"
+const CANDIDATE = "candidate"
+const LEADER_APPEND_ENTRIES = "leader_append_entries"
+const RESPONSE_APPEND_ENTRIES = "response_append_entries"
+const ENTRIES_BUFFER_SIZE = 100
+
 // global state
 var myID string
-var state = "follower"
+var leaderID string
+var state = FOLLOWER
 var currentTerm = 0
 var votedFor = ""
 var log []LogItem
@@ -47,6 +65,10 @@ var lastHeartbeatTime = time.Now()
 var ELECTION_TIMEOUT = getElectionTimeout()
 var currentTermVotes = 0
 var replicaIDs []string
+
+var entriesToAppend = make(chan LogItem, ENTRIES_BUFFER_SIZE)
+
+var committedValues = make([string]string)
 
 // change to maps
 // leader state
@@ -62,7 +84,8 @@ func main() {
 	myID = os.Args[1]
 	replicaIDs = os.Args[2:]
 
-	conn, err := net.Dial("unixpacket", myID)
+	var err error = nil
+	conn, err = net.Dial("unixpacket", myID)
 
 	if err != nil {
 		fmt.Println("TERRIBLE ERROR")
@@ -87,15 +110,31 @@ func main() {
 		switch message.Type {
 		case "get":
 			fmt.Println("get")
+			if state == LEADER {
+				// provide results
+			} else {
+				sendRedirect(message.Source)
+			}
 		case "put":
 			fmt.Println("put")
+			if state == LEADER {
+				// store results
+			} else {
+				sendRedirect(message.Source)
+			}
 		case "redirect":
-			fmt.Println("redirect")
+			fmt.Println("THIS SHOULD NEVER HAPPEN")
+			fmt.Println("SOMETHING WENT TERRIBLY WRONG")
 		case "fail":
 			fmt.Println("fail")
 		case "ok":
 			fmt.Println("ok")
 			// more to come
+		case LEADER_APPEND_ENTRIES:
+			handleAppendEntries(message)
+		case RESPONSE_APPEND_ENTRIES:
+			// TOOD: handle failures?
+			fmt.Println("ok")
 		case "requestVote":
 			if message.Term < currentTerm {
 				// send false
@@ -118,6 +157,7 @@ func main() {
 			if currentTermVotes > len(replicaIDs)/2 {
 				// we are elected leader
 				fmt.Printf("LEADER ELECTED: %v\n", myID)
+				state = LEADER
 			}
 
 		}
@@ -131,9 +171,10 @@ func checkForElection() {
 		if time.Since(lastHeartbeatTime) > ELECTION_TIMEOUT {
 			currentTerm++
 			currentTermVotes++
-			state = "candidate"
+			state = CANDIDATE
 
 			electMe := Message{
+				ID:           uuid.NewV4().String(),
 				Destination:  "FFFF",
 				Source:       myID,
 				Leader:       "FFFF",
@@ -151,8 +192,96 @@ func checkForElection() {
 	}
 }
 
+func sendAppendEntriesRpc() {
+	const HEARTBEAT_TIMEOUT = 50 * time.Millisecond
+	const HEARTBEAT_SLEEP_TIME = 25 * time.Millisecond
+	for {
+		if len(entriesToAppend) == ENTRIES_BUFFER_SIZE || time.Since(lastHeartbeatTime) > HEARTBEAT_TIMEOUT {
+
+			var actualEntries []LogItem
+
+			entriesToConsume := len(entriesToAppend)
+
+			for i := 0; i < entriesToConsume; i++ {
+				actualEntries = append(actualEntries, <-entriesToAppend)
+			}
+
+			appendEntriesMessage := Message{
+				ID:           uuid.NewV4().String(),
+				Type:         LEADER_APPEND_ENTRIES,
+				Destination:  "FFFF",
+				Source:       myID,
+				Leader:       leaderID,
+				Term:         currentTerm,
+				PrevLogIndex: lastApplied,
+				PrevLogTerm:  lastLogTerm,
+				Entries:      actualEntries,
+				LeaderCommit: commitIndex,
+			}
+
+			sendMessage(appendEntriesMessage)
+		}
+
+		time.Sleep(HEARTBEAT_SLEEP_TIME)
+	}
+}
+
+func handleAppendEntries(message Message) {
+	responseMessage := Message{
+		Source:      myID,
+		Destination: leaderID,
+		ID:          uuid.NewV4().String(),
+		Type:        RESPONSE_APPEND_ENTRIES,
+		Term:        currentTerm,
+	}
+
+	if message.Term < currentTerm {
+		responseMessage.Success = false
+	} else if len(log)-1 != message.PrevLogIndex &&
+		log[message.PrevLogIndex].Term != message.PrevLogTerm {
+		responseMessage.Success = false
+	} else if len(log)-1 > message.PrevLogIndex {
+		for i := message.PrevLogIndex; i < len(log); i++ {
+			if log[i].Term != message.Entries[i-message.PrevLogIndex].Term {
+				log = log[:i]
+				message.Entries = message.Entries[i-message.PrevLogIndex:]
+				break
+			}
+		}
+		responseMessage.Success = true
+	} else {
+		responseMessage.Success = true
+	}
+
+	if responseMessage.Success {
+		log = append(log, message.Entries...)
+		if message.LeaderCommit > commitIndex {
+			if message.LeaderCommit < len(log)-1 {
+				commitIndex = message.LeaderCommit
+			} else {
+				commitIndex = len(log) - 1
+			}
+		}
+	}
+
+	sendMessage(responseMessage)
+}
+
+func sendRedirect(destination string) {
+	redirectMessage := Message{
+		ID:          uuid.NewV4().String(),
+		Destination: destination,
+		Source:      myID,
+		Leader:      leaderID,
+		Type:        "redirect",
+	}
+
+	sendMessage(redirectMessage)
+}
+
 func sendVote(destination string, vote bool) {
 	voteMessage := Message{
+		ID:          uuid.NewV4().String(),
 		Destination: destination,
 		Source:      myID,
 		Leader:      "FFFF",
@@ -165,6 +294,12 @@ func sendVote(destination string, vote bool) {
 }
 
 func sendMessage(message Message) {
-	bytes, _ := json.Marshal(message)
+	bytes, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Printf("marshal error")
+		panic(err)
+	}
+
 	fmt.Fprintf(conn, string(bytes))
 }
